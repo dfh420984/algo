2fb7768bfde958cf5fcbda1fc503a36ea8dba667
dfh420984 <352928736@qq.com>
2019-09-30 15:20:02 +0800

    php 平方跟

--- php/15_binarysearch/binarysearch.php
+++ php/15_binarysearch/binarysearch.php
@@ -1,5 +1,6 @@
 <?php
 /**
+ * https://github.com/wangzheng0822/algo/blob/master/php/15_binary/binary.php
  * Created by PhpStorm.
  * User: duanfuhao
  * Date: 2019/9/26
@@ -10,7 +11,7 @@ function binarysearch($arr,$val) {
     if ($len <= 1) return $arr;
     $low = 0;
     $high = $len - 1;
-    $mid = int($low + ($high - $low) / 2);
+    $mid = (int)($low + ($high - $low) / 2);
     while ($low <= $high) {
         if ($arr[$mid] < $val) {
             $low = $mid + 1;
@@ -38,3 +39,36 @@ function bsearch_rec($arr,$val,$l,$h) {
         return $mid;
     }
 }
+
+/**
+ * 二分法求平方根,精确到小数点后6位
+ * @param $val
+ * @param $double
+ * @author duanfuhao@smzdm.com
+ * @date 2019/9/30
+ */
+function square($val, $double) {
+    if ($val < 0) { //必须是正数
+        return false;
+    }
+    $low = 0;
+    $up = $val;
+    if ($val > 0 && $val < 1) {
+        $low = $val;
+        $up = 1;
+    }
+    $mid = $low + ($up - $low) / 2;
+    while ($up - $low > $double) {
+        if ($mid * $mid > $val) {
+            $up = $mid;
+        } elseif ($mid * $mid < $val) {
+            $low = $mid;
+        } else {
+            return $mid;
+        }
+        $mid = $low + ($up - $low) / 2;
+    }
+    return $mid;
+}
+
+var_dump(square(9.9, 0.000001));
